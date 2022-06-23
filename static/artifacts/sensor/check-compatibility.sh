30d0b20d9e3a88d29fa61ec82dc1dde7d76a5cec
LevoBot <github-bot@levo.ai>
2022-06-23 18:13:18 +0000

    Update Sensor Compatibility Script With Latest BTF Files
    Signed-off-by: LevoBot <github-bot@levo.ai>

--- static/artifacts/sensor/check-compatibility.sh
+++ static/artifacts/sensor/check-compatibility.sh
@@ -2,41 +2,63 @@
 # Determines whether Levo.ai's eBPF Sensor can be run on the system this script is executed on.
 
 helpful_exit() {
-    printf "%s\n" "Please contact support@levo.ai for further assistance."
-    exit 1
+  printf "%s\n" "Please contact support@levo.ai for further assistance."
+  exit 1
 }
 
-IFS=" " read kernel_name kernel_version <<< "$(uname -sr)"
+IFS=" " read kernel_name kernel_version kernel_architecture <<<"$(uname -srm)"
 
 if [[ ! $kernel_name =~ Linux|GNU* ]]; then
-    printf "%s\n" "The Sensor is only compatible with Linux kernels."
-    helpful_exit
+  printf "%s\n" "The Sensor is only compatible with Linux kernels."
+  helpful_exit
+fi
+
+if [[ "$kernel_architecture" != "x86_64" ]]; then
+  printf "%s\n" "The Sensor is only compatible with 64-bit (x86_64) Linux kernels."
+  helpful_exit
 fi
 
 if grep -q "Container-Optimized OS from Google" /etc/os-release; then
-    printf "%s\n" "The Sensor is not compatible with Google's Container-Optimized OS yet."
-    helpful_exit
+  printf "%s\n" "The Sensor is not compatible with Google's Container-Optimized OS yet."
+  helpful_exit
 fi
 
-IFS=. read major minor residue <<< "${kernel_version%%*-}"
+IFS=. read major minor residue <<<"${kernel_version%%*-}"
 
 if [[ $major -lt 4 ]] || [[ $major -eq 4 && $minor -lt 14 ]]; then
-    printf "%s\n" "The Sensor is only compatible with Linux Kernel versions 4.14 and above."
-    helpful_exit
+  printf "%s\n" "The Sensor is only compatible with Linux Kernel versions 4.14 and above."
+  helpful_exit
 fi
 
 if [[ -f "/sys/kernel/btf/vmlinux" ]]; then
+  printf "%s\n" "Your system is compatible with Levo.ai's eBPF Sensor."
+  exit
+fi
+
+available_btf_kernels=(
+  "4.14.165-mikep"
+  "4.14.248-129.473.amzn1.x86_64"
+  "4.15.0-1084-fips"
+  "5.4.0-1029-aws"
+  "5.4.0-1065-gke"
+  "5.4.0-1066-gke"
+  "5.4.0-1067-gke"
+)
+for i in "${available_btf_kernels[@]}"; do
+  if [[ "$kernel_version" == "$i" ]]; then
     printf "%s\n" "Your system is compatible with Levo.ai's eBPF Sensor."
+    exit
+  fi
+done
+
+printf "%s\n" "Please contact support@levo.ai with the information below, and we will provide a custom build of the Sensor for you."
+printf "%s\n" "Diagnostic information:"
+printf "%s\n" "Kernel: $kernel_version"
+if type -p lsb_release >/dev/null; then
+  printf "%s\n" "Distribution: $(lsb_release -d -s)"
+elif [[ -r /etc/os-release ]]; then
+  printf "%s\n" "Distribution Info:"
+  printf "%s\n" "$(grep -E '(^NAME|VERSION=)' /etc/os-release |& sed 's/^/\t/')"
 else
-    printf "%s\n" "Please contact support@levo.ai with the information below, and we will provide a custom build of the Sensor for you."
-    printf "%s\n" "Diagnostic information:"
-    printf "%s\n" "Kernel: $kernel_version"
-    if type -p lsb_release >/dev/null; then
-        printf "%s\n" "Distribution: $(lsb_release -d -s)"
-    elif [[ -r /etc/os-release ]]; then
-        printf "%s\n" "Distribution Info:"
-        printf "%s\n" "$(grep -E '(^NAME|VERSION=)' /etc/os-release |& sed 's/^/\t/';)"
-    else
-        printf "%s\n" "Distribution: Unknown"
-    fi
+  printf "%s\n" "Distribution: Unknown"
 fi
