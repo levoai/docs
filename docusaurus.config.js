a1284173855aaf6f61ae39d3e8899e9fe72979e8
raviteja <gannaraputeja@gmail.com>
2024-03-13 07:03:54 -0700

    Adding redirects.

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -62,6 +62,7 @@ const config = {
       }
     ],
     [
+      // TODO: Remove this redirects after few days post new release. these client redirects are added temporarily
       '@docusaurus/plugin-client-redirects',
       {
         redirects: [
@@ -70,10 +71,140 @@ const config = {
             from: ['/api-observability/install-guide/install-satellite'],
           },
           {
+            to: '/install-traffic-capture-sensors/common-tasks/sensor-configuration',
+            from: ['/api-observability/common-tasks/sensor/sensor-configuration'],
+          },
+          {
+            to: '/integrations',
+            from: '/integrations/ticketing-notifications',
+          },
+          {
+            to: '/faqs/satellite-sensor-faqs',
+            from: '/api-observability/faqs',
+          },
+          {
             to: '/security-testing/test-laptop',
             from: ['/security-contract-testing/levo-cli/levo-cli-intro'],
           },
+          {
+            to: '/security-testing/test-laptop/test-mac-os',
+            from: '/security-contract-testing/levo-cli/levo-cli-for-mac-os',
+          },
+          {
+            to: '/security-testing/test-laptop/test-windows',
+            from: '/security-contract-testing/levo-cli/levo-cli-for-windows',
+          },
+          {
+            to: '/guides/general/os-compat-check',
+            from: '/api-observability/install-guide/os-compat-check',
+          },
+          {
+            to: '/guides/security-testing/levocli-cicd',
+            from: '/integrations/cicd-plugins',
+          },
+          {
+            to: '/install-satellite/satellite-kubernetes',
+            from: '/api-observability/common-tasks/satellite/troubleshoot',
+          },
+          {
+            to: '/guides/miscellaneous/api-integrations',
+            from: '/integrations/api-integrations',
+          },
+          {
+            to: '/guides/security-testing/common-tasks/authn-authz',
+            from: '/security-contract-testing/tasks/authentication/authn-authz',
+          },
+          {
+            to: '/install-traffic-capture-sensors/common-tasks/filter-traffic',
+            from: '/api-observability/common-tasks/sensor/filter-traffic',
+          },
+          {
+            to: '/install-satellite/satellite-kubernetes',
+            from: '/api-observability/common-tasks/satellite/satellite-mgmt',
+          },
+          {
+            to: '/guides/security-testing/test-sample-app',
+            from: '/security-contract-testing/test-sample-app/levo-sample-applications',
+          },
+          {
+            to: '/guides/miscellaneous/junit-format-results',
+            from: '/integrations/junit-format-results',
+          },
+          {
+            to: '/install-log-parsing-sensors',
+            from: '/api-observability/install-guide/install-sensor/log-parser',
+          },
+          {
+            to: '/guides/install-guide',
+            from: '/api-observability/install-guide',
+          },
+          {
+            to: '/guides/demo-application',
+            from: '/api-observability/quickstart/sample-app',
+          },
+          {
+            to: '/guides/general/private-registry',
+            from: '/api-observability/common-tasks/general/private-registry',
+          },
+          {
+            to: '/guides/key-concepts',
+            from: '/api-observability/concepts',
+          },
+          {
+            to: '/guides/api-observability',
+            from: '/api-observability',
+          },
+          {
+            to: '/guides/general/supported-platforms',
+            from: '/api-observability/supported-platforms',
+          },
+          {
+            to: '/install-api-portal',
+            from: '/api-observability/api-portal',
+          },
+          {
+            to: '/guides/general',
+            from: '/api-observability/common-tasks',
+          },
+          {
+            to: '/install-traffic-capture-sensors/ebpf-sensor/sensor-kubernetes',
+            from: '/api-observability/common-tasks/sensor/sensor-mgmt',
+          },
         ],
+        createRedirects(existingPath) {
+          if (existingPath.includes('/quickstart')) {
+            // Redirect from /docs/team/X to /community/X and /docs/support/X to /community/X
+            return [
+              existingPath.replace('/quickstart', '/api-observability/quickstart'),
+            ];
+          }
+          if (existingPath.includes('/guides')) {
+            return [
+              existingPath.replace('/guides/security-testing/concepts/', '/security-contract-testing/concepts/'),
+              existingPath.replace('/guides/security-testing/common-tasks', '/security-contract-testing/tasks'),
+              existingPath.replace('/guides/security-testing/test-your-app', '/security-contract-testing/test-your-app'),
+              existingPath.replace('/guides/security-testing/test-sample-app', '/security-contract-testing/test-sample-app'),
+              existingPath.replace('/guides/security-testing/common-tasks/authn-authz', '/security-contract-testing/tasks/authentication'),
+            ];
+          }
+          if (existingPath.includes('/security-testing')) {
+            return [
+              existingPath.replace('/security-testing/test-laptop', '/security-contract-testing/levo-cli/'),
+            ];
+          }
+          if (existingPath.includes('/install-traffic-capture-sensors')) {
+            return [
+              existingPath.replace('/install-traffic-capture-sensors', '/api-observability/install-guide/install-sensor'),
+              existingPath.replace('/install-traffic-capture-sensors/common-tasks', '/api-observability/common-tasks/sensor'),
+            ];
+          }
+          if (existingPath.includes('/quickstart')) {
+            return [
+              existingPath.replace('/quickstart', '/api-observability/quickstart'),
+            ];
+          }
+          return undefined; // Return a falsy value: no redirect created
+        },
       },
     ],
   ],
