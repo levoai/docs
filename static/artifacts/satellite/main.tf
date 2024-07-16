e96df15d1e9b933ae100b3f0ff08659eb49dc0c4
Abhishek <abhishek.rawat@levo.ai>
2024-07-16 12:48:47 +0530

    Add eCS Task role back

--- static/artifacts/satellite/main.tf
+++ static/artifacts/satellite/main.tf
@@ -309,3 +309,18 @@ resource "aws_ecs_task_definition" "levoai-satellite" {
     }
   ])
 }
+
+resource "aws_iam_role" "terraform_ecs_execution_role" {
+  name_prefix = "ecs_execution_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [{
+      Action = "sts:AssumeRole",
+      Effect = "Allow",
+      Principal = {
+        Service = "ecs-tasks.amazonaws.com",
+      },
+    }],
+  })
+}
